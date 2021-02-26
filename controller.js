fe01e6c7d4b61b4dac1bd2e62b9bfe3ecd4a5547
ariqply166 <ariqply166@gmail.com>
2021-02-26 10:36:25 +0700

    menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -2,8 +2,19 @@
 
 var response = require('./res');
 var koneksi = require('./koneksi');
+const { connect } = require('./koneksi');
 
 
 exports.index = function(req,res){
-    response.ok('Aplikasi REST API jalan !');
+    response.ok('Aplikasi REST API jalan !',res);
+};
+
+exports.datamahasiswa = function(req,res){
+    koneksi.query('SELECT * FROM mahasiswa', function(err,rows,fields){
+        if(err){
+            koneksi.log(err);
+        }else{
+            response.ok(rows, res);
+        }
+    });
 };
